66cb164d6b240250294c57d2ff062d8fde5e2d8c
aheimberger <AndreasHeimberger@gmx.at>
2023-12-16 17:57:40 +0100

    fead: add php page including bootstrap style

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Simple Web Assignment</title>
+    <link href="./style/bootstrap.min.css" rel="stylesheet">
+    </head>
+<body>
+
+    <!--
+    Todo :: Sys Call
+    -->
+
+    <script src="./js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ app/scripts/get_files.sh
@@ -0,0 +1,33 @@
+DIR_CURRENT=$(dirname "$0")
+DIR_FILES="${DIR_CURRENT}/files"
+DIR_STYLE="${DIR_CURRENT}/../css"
+DIR_JS="${DIR_CURRENT}/../js"
+
+set -e # exit on any failure (e.g. a crashing test)
+set -o pipefail # propagate a failure through pipe
+
+BOOSTRAP_VERSION="5.3.2"
+
+rm -rf "${DIR_FILES}"
+mkdir -p "${DIR_FILES}" 
+
+rm -rf "${DIR_STYLE}"
+mkdir -p "${DIR_STYLE}"
+
+rm -rf "${DIR_JS}"
+mkdir -p "${DIR_JS}"
+
+style_files="
+    bootstrap.min.css"
+
+js_files="
+    bootstrap.min.js"
+
+npm install --prefix "${DIR_FILES}" bootstrap@${BOOSTRAP_VERSION}
+for file in ${style_files}; do
+    cp "${DIR_FILES}/node_modules/bootstrap/dist/css/${file}" "${DIR_STYLE}/${file}" 
+done
+
+for file in ${js_files}; do
+    cp "${DIR_FILES}/node_modules/bootstrap/dist/js/${file}" "${DIR_JS}/${file}" 
+done
