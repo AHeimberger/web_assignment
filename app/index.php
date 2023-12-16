bceb720d9ffd7095a29756f07a9e3418f4e26ca4
aheimberger <AndreasHeimberger@gmx.at>
2023-12-16 21:11:12 +0100

    chore: echo routing table on website

--- app/index.php
+++ app/index.php
@@ -8,15 +8,57 @@
     <meta name="author" content="">
     <meta name="generator" content="Hugo 0.84.0">
     <title>Simple Web Assignment</title>
-    <link href="./style/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
     </head>
 <body>
 
-    <pre>
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Desination</th>
+        <th scope="col">Gateway</th>
+        <th scope="col">Flags</th>
+        <th scope="col">Netif</th>
+        <th scope="col">Expire</th>
+        </tr>
+    </thead>
+    <tbody>
     <?php
-        system("./service");
+        function call_service() {
+            $output = shell_exec("./service");
+            $index = 0;
+
+            foreach(preg_split("/((\r?\n)|(\r\n?))/", $output) as $line){
+                list($destination, $gateway, $flags, $netif, $expire) = explode (" ", $line);
+
+                if ($index == 0) {
+                    $index++;
+                    continue;
+                }
+
+                echo "
+                <tr>
+                    <td scope=\"row\">1</td>
+                    <td>" . $destination . "</td>
+                    <td>" . $gateway . "</td>
+                    <td>" . $flags . "</td>
+                    <td>" . $netif . "</td>
+                    <td>" . $expire . "</td>
+                </tr>
+                ";
+
+                $index++;
+            } 
+        }
+
+        call_service();
     ?>
-    </pre>
+    </tbody>
+    </table>
 
     <script src="./js/bootstrap.min.js"></script>
 </body>
